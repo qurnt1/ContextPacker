32fcc9a7a539ea17e2f670a0bc5b58393a65ad42
Quentin Chabot <quentin.chabot@etu.univ-poitiers.fr>
2026-08-12 01:07:41 +0200

    Use Material Icon Theme for file icons

--- src/components/FileTypeIcon.jsx
+++ src/components/FileTypeIcon.jsx
@@ -1,36 +1,33 @@
 import { memo } from 'react';
-import dockerIcon from 'simple-icons/icons/docker.svg?raw';
-import npmIcon from 'simple-icons/icons/npm.svg?raw';
-import gitIcon from 'simple-icons/icons/git.svg?raw';
-import javascriptIcon from 'simple-icons/icons/javascript.svg?raw';
-import reactIcon from 'simple-icons/icons/react.svg?raw';
-import typescriptIcon from 'simple-icons/icons/typescript.svg?raw';
-import pythonIcon from 'simple-icons/icons/python.svg?raw';
-import rustIcon from 'simple-icons/icons/rust.svg?raw';
-import goIcon from 'simple-icons/icons/go.svg?raw';
-import javaIcon from 'simple-icons/icons/openjdk.svg?raw';
-import vueIcon from 'simple-icons/icons/vuedotjs.svg?raw';
-import svelteIcon from 'simple-icons/icons/svelte.svg?raw';
-import terraformIcon from 'simple-icons/icons/terraform.svg?raw';
-import markdownIcon from 'simple-icons/icons/markdown.svg?raw';
-import yamlIcon from 'simple-icons/icons/yaml.svg?raw';
-import {
-  FileCog,
-  FileJson,
-  FileText,
-  FileType2,
-  ListTree,
-} from 'lucide-react';
+import dockerIcon from 'material-icon-theme/icons/docker.svg?raw';
+import npmIcon from 'material-icon-theme/icons/npm.svg?raw';
+import gitIcon from 'material-icon-theme/icons/git.svg?raw';
+import javascriptIcon from 'material-icon-theme/icons/javascript.svg?raw';
+import reactIcon from 'material-icon-theme/icons/react.svg?raw';
+import typescriptIcon from 'material-icon-theme/icons/typescript.svg?raw';
+import pythonIcon from 'material-icon-theme/icons/python.svg?raw';
+import rustIcon from 'material-icon-theme/icons/rust.svg?raw';
+import goIcon from 'material-icon-theme/icons/go.svg?raw';
+import javaIcon from 'material-icon-theme/icons/java.svg?raw';
+import vueIcon from 'material-icon-theme/icons/vue.svg?raw';
+import svelteIcon from 'material-icon-theme/icons/svelte.svg?raw';
+import terraformIcon from 'material-icon-theme/icons/terraform.svg?raw';
+import markdownIcon from 'material-icon-theme/icons/markdown.svg?raw';
+import yamlIcon from 'material-icon-theme/icons/yaml.svg?raw';
+import jsonIcon from 'material-icon-theme/icons/json.svg?raw';
+import cssIcon from 'material-icon-theme/icons/css.svg?raw';
+import sassIcon from 'material-icon-theme/icons/sass.svg?raw';
+import lessIcon from 'material-icon-theme/icons/less.svg?raw';
+import htmlIcon from 'material-icon-theme/icons/html.svg?raw';
+import readmeIcon from 'material-icon-theme/icons/readme.svg?raw';
+import licenseIcon from 'material-icon-theme/icons/license.svg?raw';
+import tsconfigIcon from 'material-icon-theme/icons/tsconfig.svg?raw';
+import viteIcon from 'material-icon-theme/icons/vite.svg?raw';
+import vitestIcon from 'material-icon-theme/icons/vitest.svg?raw';
+import textIcon from 'material-icon-theme/icons/document.svg?raw';
+import configIcon from 'material-icon-theme/icons/settings.svg?raw';
+import genericIcon from 'material-icon-theme/icons/file.svg?raw';
 import { getFileTypeInfo } from '../utils/languageBadge';
-
-const TYPE_ICONS = {
-  json: FileJson,
-  markdown: FileText,
-  yaml: ListTree,
-  text: FileText,
-  config: FileCog,
-  generic: FileType2,
-};
 
 const TYPE_BRAND_ICONS = {
   docker: dockerIcon,
@@ -48,6 +45,19 @@ const TYPE_BRAND_ICONS = {
   terraform: terraformIcon,
   markdown: markdownIcon,
   yaml: yamlIcon,
+  json: jsonIcon,
+  css: cssIcon,
+  scss: sassIcon,
+  less: lessIcon,
+  html: htmlIcon,
+  readme: readmeIcon,
+  license: licenseIcon,
+  tsconfig: tsconfigIcon,
+  vite: viteIcon,
+  vitest: vitestIcon,
+  text: textIcon,
+  config: configIcon,
+  generic: genericIcon,
 };
 
 function getBrandMarkup(rawSvg) {
@@ -56,35 +66,18 @@ function getBrandMarkup(rawSvg) {
 
 const FileTypeIcon = memo(function FileTypeIcon({ fileName, extension, size = 15, className = '' }) {
   const fileType = getFileTypeInfo(fileName, extension);
-  const brandIcon = TYPE_BRAND_ICONS[fileType.type];
-  const Icon = TYPE_ICONS[fileType.type] || FileType2;
-
-  if (brandIcon) {
-    return (
-      <svg
-        aria-hidden="true"
-        className={`flex-shrink-0 ${className}`}
-        data-file-type={fileType.type}
-        height={size}
-        viewBox="0 0 24 24"
-        width={size}
-        fill="currentColor"
-        style={{ color: fileType.color }}
-        title={fileType.label}
-        dangerouslySetInnerHTML={{ __html: getBrandMarkup(brandIcon) }}
-      />
-    );
-  }
+  const icon = TYPE_BRAND_ICONS[fileType.type] || genericIcon;
 
   return (
-    <Icon
+    <svg
       aria-hidden="true"
       className={`flex-shrink-0 ${className}`}
       data-file-type={fileType.type}
-      size={size}
-      strokeWidth={1.8}
-      style={{ color: fileType.color }}
+      height={size}
+      viewBox="0 0 16 16"
+      width={size}
       title={fileType.label}
+      dangerouslySetInnerHTML={{ __html: getBrandMarkup(icon) }}
     />
   );
 });
